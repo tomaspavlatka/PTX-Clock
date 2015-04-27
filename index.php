b563a5c5a61bd3665f5406ab94bff747954c5dbd
Tomas Pavlatka <tomas@pavlatka.cz>
2015-04-27 03:04:00 +0300

    backup

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ require_once './vendor/autoload.php';
 try {
     $base = new PTX\ClockCanvas();
     $base->draw();
-    $file_path = './cache/base.png';
     $base->to_browser();
 } catch(\PTX\ClockException $e) {
     echo $e->getMessage();
